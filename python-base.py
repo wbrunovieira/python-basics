5e7b29823a27fb6c5ca03b8cd7bb0d149e612b1c
wbrunovieira <wbrunovieira77@gmail.com>
2023-10-10 19:39:19 -0300

    first commit

---
+++ python-base.py
@@ -0,0 +1,38 @@
+a = 2
+b = 4
+
+codigo_py = 'o melhor curso de python brasil' 
+print(a)
+print(b)
+print(codigo_py)
+
+inteiro = int(5)
+flutuante_ou_decimal = float(1.5) 
+string = str("codigo.py")
+string2 = str('curso de python') 
+boleano = bool(True)
+boleano2 = bool(False) 
+
+print(type(inteiro))
+print(inteiro)
+
+string_normal = "A melhor empresa do Brasil é a Weg"
+
+empresa = "Petrobras"
+
+string_variavel = f"A melhor empresa do Brasil é a {empresa}, {empresa}"
+
+numero_digitado = int(input('Digite um número inteiro: '))
+
+numero_final = numero_digitado + 3
+
+print(numero_final)
+
+print(string_variavel)
+
+nome = input('Digite o nome da pessoa mais rica do mundo: ')
+
+string_pessoa_rica  = f'{nome} é a pessoa mais rica do mundo!'
+
+print(string_pessoa_rica)
+
